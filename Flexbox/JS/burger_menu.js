af25c5c2d76db6f62841c41581a90993c5b333de
Philip Bower <philbower80@outlook.com>
2020-08-03 08:59:07 +0100

    custom checkbox added

--- Flexbox/JS/burger_menu.js
+++ Flexbox/JS/burger_menu.js
@@ -1,11 +1,11 @@
 const burgerMenu = document.querySelectorAll('.burger-container');
-// const overlay = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.nav-overlay');
 
 for (let i = 0; i < burgerMenu.length; i++) {
 	let menu = burgerMenu[i];
 
 	menu.addEventListener('click', () => {
-		// overlay.classList.toggle('active');
+		overlay.classList.toggle('active');
 		for (let i = 0; i < burgerMenu.length; i++) {
 			burgerMenu[i].classList.toggle('active');
 		}
