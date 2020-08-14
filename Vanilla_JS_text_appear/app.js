58c07c5028ca4ccf3d43a6837789cd0f86d8f719
Philip Bower <philbower80@outlook.com>
2020-08-14 22:21:14 +0100

    appear effect updated

--- Vanilla_JS_text_appear/app.js
+++ Vanilla_JS_text_appear/app.js
@@ -1,7 +1,6 @@
 function scrollAppear() {
 	let introText = document.querySelector('.intro-text');
-	let boundBox = document.querySelector('.intro-text-bounding-box');
-	let introPosition = boundBox.getBoundingClientRect().top;
+	let introPosition = introText.getBoundingClientRect().top;
 	let screenPosition = window.innerHeight / 1.3;
 
 	if (introPosition < screenPosition) {
